7cd7083db0360daa847b6255692e21987ac66ea7
unknown <vrublevskiy.2015@gmail.com>
2024-08-07 14:24:39 +0300

    cron job for clearing old records

---
+++ tasks/clear_db.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta
+from celery.utils.log import get_task_logger
+
+from data_access.repository import Repository
+from data_access.create_connection import create_connection
+from celery_settings.celery_config import app
+from settings.config import CONFIG
+
+repository = Repository(create_connection(CONFIG.POSTGRE_CONNECTION_STR))
+
+
+logger = get_task_logger(__name__)
+
+@app.task
+def clear_old_files():
+    logger.info("Task ran")
+
+    current_time = datetime.now()
+    timestamp = current_time - timedelta(days=1)
+    
+    error = repository.delete_old_records(timestamp)
+    if error:
+        logger.error(error)
+    logger.info("Task done")
