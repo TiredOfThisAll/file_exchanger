1a50295c1f1f1bd5ce4cb293a859b76d61b398aa
unknown <vrublevskiy.2015@gmail.com>
2024-08-07 14:23:01 +0300

    config files

---
+++ settings/config.py
@@ -0,0 +1,18 @@
+import json
+import os
+
+
+class Config:
+    def __init__(self) -> None:
+        self.PROJECT_PATH = os.getcwd()
+
+        with open(os.path.join(self.PROJECT_PATH, "settings", "config.json")) as f:
+            config_dict = json.load(f)
+
+        self.POSTGRE_CONNECTION_STR = config_dict["postgre_connection_str"]
+        self.SQLITE_CONNECTION_STR = config_dict["sqlite_connection_str"]
+
+        # the path must be relative to the root folder of the project
+        self.FILES_PATH = os.path.join(self.PROJECT_PATH, config_dict["files_path"])
+
+CONFIG = Config()
